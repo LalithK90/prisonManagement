23033646bd1f007c6862b7672df0d1f932296db1
LalithK90 <asakahatapitiya@gmail.com>
2020-07-13 22:17:26 +0530

    Initial commit

---
+++ src/main/java/lk/prisonManagement/PrisonManagementApplication.java
@@ -0,0 +1,13 @@
+package lk.prisonManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrisonManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrisonManagementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/lk/prisonManagement/asset/message/dao/EmailMessageDao.java
@@ -0,0 +1,9 @@
+package lk.prisonManagement.asset.message.dao;
+
+import lk.prisonManagement.asset.message.entity.EmailMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmailMessageDao extends JpaRepository<EmailMessage, Integer > {
+}
