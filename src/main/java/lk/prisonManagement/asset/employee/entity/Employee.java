97b00f16a9b4cbc4d4d6a745c249d1452f7c7ae6
Hansi <hasi.munasinghe123@gmail.com>
2020-07-15 23:34:29 +0530

    fg

--- src/main/java/lk/prisonManagement/asset/employee/entity/Employee.java
+++ src/main/java/lk/prisonManagement/asset/employee/entity/Employee.java
@@ -1,12 +1,13 @@
 package lk.prisonManagement.asset.employee.entity;
 
+
+
 import com.fasterxml.jackson.annotation.JsonFilter;
 import lk.prisonManagement.asset.commonAsset.model.Enum.BloodGroup;
 import lk.prisonManagement.asset.commonAsset.model.Enum.CivilStatus;
 import lk.prisonManagement.asset.commonAsset.model.Enum.Gender;
 import lk.prisonManagement.asset.commonAsset.model.Enum.Title;
 import lk.prisonManagement.asset.commonAsset.model.FileInfo;
-import lk.prisonManagement.asset.employee.entity.Enum.Designation;
 import lk.prisonManagement.asset.employee.entity.Enum.EmployeeStatus;
 import lk.prisonManagement.asset.message.entity.EmailMessage;
 import lk.prisonManagement.util.audit.AuditEntity;
@@ -14,6 +15,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.dom4j.Branch;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,79 +25,81 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonFilter( "Employee" )
-public class Employee extends AuditEntity {
+@JsonFilter("Employee")
+public class Employee<Designation> extends AuditEntity {
 
-    @Column( unique = true )
+    @Column(unique = true)
     private String payRoleNumber;
 
-    @Size( min = 5, message = "Your name cannot be accepted" )
+    @Size(min = 5, message = "Your name cannot be accepted")
     private String name;
 
-    @Size( min = 5, message = "At least 5 characters should be included calling name" )
+    @Size(min = 5, message = "At least 5 characters should be included calling name")
     private String callingName;
 
-    @Size( max = 12, min = 10, message = "NIC number is contained numbers between 9 and X/V or 12 " )
-    @Column( unique = true )
+    @Size(max = 12, min = 10, message = "NIC number is contained numbers between 9 and X/V or 12 ")
+    @Column(unique = true)
     private String nic;
 
-    @Column( unique = true )
+    @Column(unique = true)
     private String departmentIdNumber;
 
-    @Size( max = 10, message = "Mobile number length should be contained 10 and 9" )
+    @Size(max = 10, message = "Mobile number length should be contained 10 and 9")
     private String mobileOne;
 
     private String mobileTwo;
 
     private String land;
 
-    @Column( unique = true )
+    @Column(unique = true)
     private String email;
 
-    @Column( unique = true )
+    @Column(unique = true)
     private String officeEmail;
 
-    @Column( columnDefinition = "VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NULL", length = 255 )
+    @Column(columnDefinition = "VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NULL", length = 255)
     private String address;
 
-    @Enumerated( EnumType.STRING )
+    @Enumerated(EnumType.STRING)
     private Title title;
 
-    @Enumerated( EnumType.STRING )
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    @Enumerated( EnumType.STRING )
+    @Enumerated(EnumType.STRING)
     private BloodGroup bloodGroup;
 
-    @Enumerated( EnumType.STRING )
+    @Enumerated(EnumType.STRING)
     private Designation designation;
 
-    @Enumerated( EnumType.STRING )
+    @Enumerated(EnumType.STRING)
     private CivilStatus civilStatus;
 
-    @Enumerated( EnumType.STRING )
+    @Enumerated(EnumType.STRING)
     private EmployeeStatus employeeStatus;
 
-    @DateTimeFormat( pattern = "yyyy-MM-dd" )
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
 
-    @DateTimeFormat( pattern = "yyyy-MM-dd" )
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfAssignment;
 
 
-    @ManyToMany( mappedBy = "employees" )
+
+    @ManyToMany(mappedBy = "employees")
     private List<EmailMessage> emailMessages;
 
     @Transient
-    private List< MultipartFile > files = new ArrayList<>();
+    private List<MultipartFile> files = new ArrayList<>();
 
     @Transient
-    private List< String > removeImages = new ArrayList<>();
+    private List<String> removeImages = new ArrayList<>();
 
     @Transient
     private List<FileInfo> fileInfos = new ArrayList<>();
--- src/main/java/lk/prisonManagement/asset/employee/entity/Enum/EmployeeStatus.java
+++ src/main/java/lk/prisonManagement/asset/employee/entity/Enum/EmployeeStatus.java
@@ -1,5 +1,7 @@
 package lk.prisonManagement.asset.employee.entity.Enum;
 
+
+
 public enum EmployeeStatus {
     WORKING("Working"),
     LEAVE("Leave"),
