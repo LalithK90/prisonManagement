bde794282fbb4f8c111ed0bcc7ae6f9ef40cb428
LalithK90 <asakahatapitiya@gmail.com>
2020-12-17 00:46:48 +0530

    EmployeeQualification was updated

--- src/main/java/lk/prison_management/asset/employee_qualification/entity/EmployeeQualification.java
+++ src/main/java/lk/prison_management/asset/employee_qualification/entity/EmployeeQualification.java
@@ -1,17 +1,16 @@
 package lk.prison_management.asset.employee_qualification.entity;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import lk.prison_management.asset.employee.entity.Employee;
 import lk.prison_management.asset.employee_qualification.entity.enums.EmployeeQualificationStatus;
+import lk.prison_management.asset.qualification.entity.Qualification;
 import lk.prison_management.util.audit.AuditEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -22,6 +21,8 @@ import java.time.LocalDateTime;
 @JsonFilter( "EmployeeQualification" )
 public class EmployeeQualification extends AuditEntity {
 
+  private String instituteName;
+
   @Enumerated( EnumType.STRING)
   private EmployeeQualificationStatus employeeQualificationStatus;
 
@@ -30,4 +31,10 @@ public class EmployeeQualification extends AuditEntity {
 
   @Column( nullable = false )
   private LocalDateTime  durationEndAt;
+
+  @ManyToOne
+  private Employee employee;
+
+  @ManyToOne
+  private Qualification qualification;
 }
