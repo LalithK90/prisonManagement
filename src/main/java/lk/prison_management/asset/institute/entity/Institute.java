e9c3a4c21922d2a00c8936b39dc261aec0476935
LalithK90 <asakahatapitiya@gmail.com>
2020-12-17 00:06:05 +0530

    Institute related content was updated

--- src/main/java/lk/prison_management/asset/institute/entity/Institute.java
+++ src/main/java/lk/prison_management/asset/institute/entity/Institute.java
@@ -1,6 +1,7 @@
 package lk.prison_management.asset.institute.entity;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import lk.prison_management.asset.employee_institute.entity.EmployeeInstitute;
 import lk.prison_management.asset.institute.entity.enums.PrisonType;
 import lk.prison_management.util.audit.AuditEntity;
 import lombok.AllArgsConstructor;
@@ -8,29 +9,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonFilter("Institute")
+@JsonFilter( "Institute" )
 public class Institute extends AuditEntity {
 
-    private String name;
+  private String name;
 
-    @Column(unique = true)
-    private String land;
+  @Column( unique = true )
+  private String land;
 
-    private String address;
+  private String address;
 
-    @Column(unique = true)
-    private String email;
+  @Column( unique = true )
+  private String email;
 
-    @Enumerated(EnumType.STRING)
-    private PrisonType prisonType;
+  @Enumerated( EnumType.STRING )
+  private PrisonType prisonType;
+
+  @OneToMany(mappedBy = "institute")
+  private List< EmployeeInstitute > employeeInstitutes;
 }
--- src/main/java/lk/prison_management/asset/institute/entity/enums/InstituteChangeReason.java
+++ src/main/java/lk/prison_management/asset/institute/entity/enums/InstituteChangeReason.java
@@ -1,10 +1,10 @@
-package lk.prison_management.asset.employee.entity.enums;
+package lk.prison_management.asset.institute.entity.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum WorkingPlaceChangeReason {
+public enum InstituteChangeReason {
   IMPORTANCEOFSERVICE("Importance of Service"),
   EMLOYEEREQUEST("Employee Request"),
   DISIPILINARYACTION("Disciplinary Action"),
