885b35fef7ced3d0b10d7f24032118f6a530c5b3
LalithK90 <asakahatapitiya@gmail.com>
2021-01-03 06:14:28 +0530

    Remove all th: legacy thymeleaf code to html 5 tag

---
+++ src/main/java/lk/prison_management/asset/employee_institute/controller/EmployeeInstituteController.java
@@ -0,0 +1,9 @@
+package lk.prison_management.asset.employee_institute.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employeeInstitute")
+public class EmployeeInstituteController {
+}
