630df6c147a642eb8f7f216773159a6d9b7868ed
LalithK90 <asakahatapitiya@gmail.com>
2021-01-11 21:40:38 +0530

    Performance evaluation was started

--- src/main/java/lk/prison_management/asset/process_management/proformance_evaluation/entity/Proformance_evaluation.java
+++
@@ -1,4 +0,0 @@
-package lk.prison_management.asset.process_management.proformance_evaluation.entity;
-
-public class Proformance_evaluation {
-}
---
+++ src/main/java/lk/prison_management/asset/process_management/proformance_evaluation/entity/enums/PerformanceEvaluationStatus.java
@@ -0,0 +1,13 @@
+package lk.prison_management.asset.process_management.proformance_evaluation.entity.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PerformanceEvaluationStatus {
+
+  Approved(" Approved");
+  private final String performanceEvaluationStatus;
+
+}
