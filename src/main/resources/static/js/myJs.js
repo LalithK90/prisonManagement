f6876a19b3247848d5dad60714509e46448ab3c3
Hansi <hasi.munasinghe123@gmail.com>
2021-04-22 20:10:04 +0530

    search and pagination fixed

--- src/main/resources/static/js/myJs.js
+++ src/main/resources/static/js/myJs.js
@@ -551,3 +551,9 @@ $(".reveal").on('click', function () {
         $pwd.attr('type', 'password');
     }
 });
+$("#myTable").DataTable({
+    "lengthMenu": [[5, 10, 15, 20, -1], [5, 10, 15, 20, "All"]],
+    "ordering": false,
+    stateSave: true,
+});
+
